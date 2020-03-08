a0ee83dfa7512cc3c2907a710f9695e27a1448e6
Paul <pxj1314@hotmail.com>
2020-03-08 18:00:46 +0800

    创建person文件

---
+++ cocoapodgittest/cocoapodgittest/person.h
@@ -0,0 +1,17 @@
+//
+//  person.h
+//  cocoapodgittest
+//
+//  Created by Paul on 2020/3/8.
+//  Copyright © 2020 Paul. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface person : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ cocoapodgittest/cocoapodgittest/person.m
@@ -0,0 +1,14 @@
+
+//
+//  person.m
+//  cocoapodgittest
+//
+//  Created by Paul on 2020/3/8.
+//  Copyright © 2020 Paul. All rights reserved.
+//
+
+#import "person.h"
+
+@implementation person
+
+@end
